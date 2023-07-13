ab81e9b3857167c57447e91a60fa5477148bef29
Diego Quimbo <dquimbo22@gmail.com>
2023-07-13 07:30:54 -0300

    new dependecies

--- Package.swift
+++ Package.swift
@@ -3,18 +3,24 @@
 import PackageDescription
 
 let package = Package(
-  name: "PBBluetooth",
-  platforms: [
-    .iOS(.v13)
-  ],
-  products: [
-    .library(
-        name: "PBBluetooth",
-        targets: ["PBBluetooth"]),
-  ],
-  targets: [
-    .binaryTarget(
-        name: "PBBluetooth",
-        path: "./Sources/PBBluetooth.xcframework")
-  ]
+    name: "PBBluetooth",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "PBBluetooth",
+            targets: ["PBBluetooth"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager",
+            from: "1.0.0"
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "PBBluetooth",
+            path: "./Sources/PBBluetooth.xcframework")
+    ]
 )
