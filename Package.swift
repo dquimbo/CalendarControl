58bbfe6efcef5f14483240b7725ce9549bd29668
Diego Quimbo <dquimbo22@gmail.com>
2023-07-13 09:56:46 -0300

    add dependency

--- Package.swift
+++ Package.swift
@@ -24,6 +24,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SimpleTest",
-            dependencies: []),
+            dependencies: [
+                .product(name: "iOSMcuManagerLibrary", package: "IOS-nRF-Connect-Device-Manager")
+            ]
+        ),
     ]
 )
