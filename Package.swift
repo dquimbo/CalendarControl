48f201d3991bfbec7506d6fcd5c2e1e128060ccb
Diego Quimbo <dquimbo22@gmail.com>
2023-07-13 11:30:26 -0300

    new package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PBBluetooth",
-            targets: ["PBBluetooth"]),
+            targets: ["PBBluetoothWrapper"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,15 +22,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "PBBluetooth",
+            path: "./Sources/PBBluetooth.xcframework"),
+        .target(
+            name: "PBBluetoothWrapper",
             dependencies: [
                 .product(name: "iOSMcuManagerLibrary", package: "IOS-nRF-Connect-Device-Manager"),
-                .target(name: "PBBluetoothBinary")
+                .target(name: "PBBluetooth")
             ]
         ),
-        .binaryTarget(
-            name: "PBBluetoothBinary",
-            path: "./Sources/PBBluetooth.xcframework"),
     ]
 )
---
+++ Sources/PBBluetoothWrapper/WebServiceT.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Diego Quimbo on 13/7/23.
+//
+
+import Foundation
+
+public class WebServiceTestsD {
+    let property1: String
+    
+    public init(property1: String) {
+        self.property1 = property1
+    }
+}
