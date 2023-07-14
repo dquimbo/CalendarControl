c1de0193cc074a2bcd536dcd6f4624affc9c9997
Diego Quimbo <dquimbo22@gmail.com>
2023-07-14 16:18:04 -0300

    new version

--- Package.swift
+++ Package.swift
@@ -1,41 +1,24 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "PBBluetooth",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "PBBluetooth",
-            targets: ["PBBluetoothWrapper"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager",
-            from: "1.0.0"
-        ),
-        .package(
-            url: "https://github.com/ReactiveX/RxSwift",
-            from: "6.0.0"
-        )
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-            name: "PBBluetooth",
-            path: "./Sources/PBBluetooth.xcframework"),
-        .target(
-            name: "PBBluetoothWrapper",
-            dependencies: [
-                .product(name: "iOSMcuManagerLibrary", package: "IOS-nRF-Connect-Device-Manager"),
-                .product(name: "RxSwift", package: "rxswift"),
-                .target(name: "PBBluetooth")
-            ]
-        ),
-    ]
+  name: "PBBluetooth",
+  products: [
+    .library(
+        name: "PBBluetooth",
+        targets: ["PBBluetooth"]),
+  ],
+  dependencies: [
+      // Dependencies declare other packages that this package depends on.
+      .package(
+          url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager",
+          from: "1.0.0"
+      )
+  ],
+  targets: [
+    .binaryTarget(
+        name: "PBBluetooth",
+        path: "./Sources/PBBluetooth.xcframework")
+  ]
 )
--- README.md
+++
@@ -1,3 +0,0 @@
-# PBBluetooth
-
-A description of this package.
